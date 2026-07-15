ec6a1bba2ccd4fe2b8c50e905a03d3e4f62d788b
Minijoe01 <minijoe01@hotmail.fr>
2026-07-15 20:27:36 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Trakt Smart Lists", page_icon="🎬")
+
+st.title("🎬 Trakt Smart Lists")
+st.write("Ça fonctionne ! On va construire la vraie appli ici, étape par étape.")
